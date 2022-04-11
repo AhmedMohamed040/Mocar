fc589fd56778880523af46a00f6e4beddad9b2a2
AhmedMohamed040 <doompop40@gmail.com>
2022-04-11 12:40:30 +0200

    the project files

---
+++ js/Mocar.js
@@ -0,0 +1,28 @@
+
+
+  let lader = document.getElementById('preloader');
+  let nav = document.getElementById('navbar');
+  
+  
+
+
+
+  window.addEventListener('scroll', function() {
+    if(window.scrollY > 50) {
+      nav.classList.add('top-nav-collapse');
+    }else {
+      nav.classList.remove('top-nav-collapse');
+    }
+});
+
+  
+
+  AOS.init();
+
+  
+
+
+ setTimeout(function() {
+  lader.remove();
+
+}, 200);
